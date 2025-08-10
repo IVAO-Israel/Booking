7c8fdaf1b52ba1e67cc87e4ff1a6d816a00bd520
ivaoil <borna.cesarec@ivao.aero>
2025-08-10 14:50:30 +0200

    Created EventAtcPosition

---
+++ Booking/Data/EventAtcPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking.Data
+{
+    /// <summary>
+    /// ATC position available for booking during given event and time.
+    /// </summary>
+    public class EventAtcPosition
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public required int EventId { get; set; }
+        public required Event Event { get; set; }
+        public required Guid AtcPositionId { get; set; }
+        public required AtcPosition AtcPosition { get; set; }
+        public required DateTime BegingTime { get; set; }
+        public required DateTime EndTime { get; set; }
+    }
+}
