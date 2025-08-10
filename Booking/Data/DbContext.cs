05b75d48158913e857c2b936b0565083ac3c66dc
ivaoil <borna.cesarec@ivao.aero>
2025-08-10 14:52:35 +0200

    Created DbContext and relations for models

---
+++ Booking/Data/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.Data
+{
+    public class BookingDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            CreateRelations(modelBuilder);
+        }
+        private void CreateRelations(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EventAtcPosition>().HasOne(p => p.Event).WithMany(p => p.AvailableAtcPositions)
+                                                   .HasForeignKey(p => p.EventId);
+            modelBuilder.Entity<EventAtcPosition>().HasOne(p => p.AtcPosition).WithMany().HasForeignKey(p => p.AtcPositionId);
+        }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<AtcPosition> AtcPositions { get; set; }
+        public DbSet<EventAtcPosition> EventAtcPositions { get; set; }
+    }
+}
