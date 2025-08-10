30aa1987955849afc26c30e7616b64c0834e222f
ivaoil <borna.cesarec@ivao.aero>
2025-08-10 14:42:03 +0200

    Event model created

---
+++ Booking/Data/Event.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Booking.Data
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required DateTime BeginTime { get; set; }
+        public required DateTime EndTime { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
