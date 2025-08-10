85a785a6b0c5e2001ac49ca46b2402ae4ddc95a8
ivaoil <borna.cesarec@ivao.aero>
2025-08-10 14:42:19 +0200

    AtcPosition model created

---
+++ Booking/Data/AtcPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking.Data
+{
+    public class AtcPosition
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public required string IVAOCallsign { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+    }
+}
