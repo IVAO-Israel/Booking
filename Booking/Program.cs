c310a88e42b28172fd815528edf6a0be835164a7
ivaoil <borna.cesarec@ivao.aero>
2025-08-11 16:04:36 +0200

    Update for authentication and authorization

--- Booking/Program.cs
+++ Booking/Program.cs
@@ -1,5 +1,12 @@
-using Booking.Client.Pages;
+using System.Security.Claims;
+using Booking.Authorization;
 using Booking.Components;
+using Booking.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Booking
 {
@@ -13,6 +20,56 @@ namespace Booking
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents()
                 .AddInteractiveWebAssemblyComponents();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<BookingDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            // Authentication setup
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.Authority = "https://api.ivao.aero";
+                options.ClientId = builder.Configuration["OpenId:ClientId"];
+                options.ClientSecret = builder.Configuration["OpenId:ClientSecret"];
+                options.ResponseType = "code";
+                options.SaveTokens = true;
+
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("email");
+                // Add other scopes as needed
+
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = ClaimTypes.Role,
+                    ValidAlgorithms = [SecurityAlgorithms.RsaSha512]
+                };
+
+                options.CallbackPath = "/signin-oidc";
+                options.SignedOutCallbackPath = "/signout-callback-oidc";
+
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnTokenValidated = async ctx =>
+                    {
+                        await Task.CompletedTask;
+                    }
+                };
+            });
+
+            // Authorization
+            builder.Services.AddAuthorizationBuilder().AddPolicy("Administrator", policy => policy.Requirements.Add(new AdministratorRequirement()));
+
+            // Register handler for Administrator protection
+            builder.Services.AddScoped<IAuthorizationHandler, AdministratorAuthorizationHandler>();
 
             var app = builder.Build();
 
@@ -31,6 +88,9 @@ namespace Booking
             app.UseHttpsRedirection();
 
             app.UseAntiforgery();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapStaticAssets();
             app.MapRazorComponents<App>()
