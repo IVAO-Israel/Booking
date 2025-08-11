fef97b588d2ec1edb69a1fa514909f68f2fe8608
ivaoil <borna.cesarec@ivao.aero>
2025-08-11 08:13:23 +0200

    Using integer for IVAOUserId

--- Booking/Data/AtcPositionBooking.cs
+++ Booking/Data/AtcPositionBooking.cs
@@ -13,6 +13,6 @@ namespace Booking.Data
         public EventAtcPosition EventAtcPosition { get; set; } = default!;
         public DateTime BeginTime { get; set; }
         public DateTime EndTime { get; set; }
-        public int IVAOUserId { get; set; } //TODO: choose int or something else, 6 digit should fit, but not sure if any is 0xxxxx
+        public int IVAOUserId { get; set; }
     }
 }
