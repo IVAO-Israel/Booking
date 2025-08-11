84114fe89fda008b108b2b54b9fa6c6aab9ad0ee
ivaoil <borna.cesarec@ivao.aero>
2025-08-11 16:24:58 +0200

    Added missing constructor

--- Booking/Data/BookingDbContext.cs
+++ Booking/Data/BookingDbContext.cs
@@ -4,6 +4,9 @@ namespace Booking.Data
 {
     public class BookingDbContext : DbContext
     {
+        public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
+        {
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
