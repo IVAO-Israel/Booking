31e2d6efe96081894602a155c48635150a1861b3
ivaoil <borna.cesarec@ivao.aero>
2025-08-12 07:14:55 +0200

    Added AdministratorService for CRUD

---
+++ Booking/Services/AdministratorService.cs
@@ -0,0 +1,35 @@
+﻿using Booking.Data;
+using Booking.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.Services
+{
+    public class DbAdministratorService (BookingDbContext dbContext) : IAdministratorService
+    {
+        private readonly BookingDbContext _dbContext = dbContext;
+        void IAdministratorService.AddAdministrator(Administrator administrator)
+        {
+            _dbContext.Entry(administrator).State = EntityState.Added;
+        }
+        void IAdministratorService.RemoveAdministrator(Administrator administrator)
+        {
+            if(_dbContext.Entry(administrator).State == EntityState.Added){
+                _dbContext.Entry(administrator).State = EntityState.Detached;
+            } else
+            {
+                _dbContext.Entry(administrator).State = EntityState.Deleted;
+            }
+        }
+        async Task<Administrator?> IAdministratorService.GetAdministrator(int IVAOUserId)
+        {
+            return await _dbContext.Administrators.Where(a => a.IVAOUserId  == IVAOUserId).FirstOrDefaultAsync();
+        }
+        void IAdministratorService.UpdateAdministrator(Administrator administrator)
+        {
+            if (_dbContext.Entry(administrator).State != EntityState.Added)
+            {
+                _dbContext.Entry(administrator).State = EntityState.Modified;
+            }
+        }
+    }
+}
---
+++ Booking/Services/Interfaces/IAdministratorService.cs
@@ -0,0 +1,12 @@
+﻿using Booking.Data;
+
+namespace Booking.Services.Interfaces
+{
+    public interface IAdministratorService
+    {
+        public void AddAdministrator(Administrator administrator);
+        public Task<Administrator?> GetAdministrator(int IVAOUserId);
+        public void UpdateAdministrator(Administrator administrator);
+        public void RemoveAdministrator(Administrator administrator);
+    }
+}
