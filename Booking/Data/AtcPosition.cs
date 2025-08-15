89f1e3e6328159dbafcd1c6ab78eda867fefcb0b
ivaoil <borna.cesarec@ivao.aero>
2025-08-15 11:18:51 +0200

    Override  to string

--- Booking/Data/AtcPosition.cs
+++ Booking/Data/AtcPosition.cs
@@ -12,5 +12,9 @@ namespace Booking.Data
         public required string IVAOCallsign { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} ({IVAOCallsign})";
+        }
     }
 }
