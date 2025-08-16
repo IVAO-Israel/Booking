23102f7395876a98441ee05f02a04184c5d8f247
ivaoil <163436202+ivaoil@users.noreply.github.com>
2025-08-16 13:37:08 +0200

    Revert "Override  to string"
    This reverts commit 392f301918f0aea84a02331d70cc9ea67655a596.

--- Booking/Data/AtcPosition.cs
+++ Booking/Data/AtcPosition.cs
@@ -12,9 +12,5 @@ namespace Booking.Data
         public required string IVAOCallsign { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
-        public override string ToString()
-        {
-            return $"{Name} ({IVAOCallsign})";
-        }
     }
 }
