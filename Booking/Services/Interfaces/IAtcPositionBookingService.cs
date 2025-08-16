03b3ca30c809d205be5aa467fc81bfcda8550cdb
ivaoil <163436202+ivaoil@users.noreply.github.com>
2025-08-16 13:37:08 +0200

    Revert "AtcPositionbooking service using dbcontext factory"
    This reverts commit 59d9bd87bcd976d86403ee126fcbaa7f39115a24.

--- Booking/Services/Interfaces/IAtcPositionBookingService.cs
+++ Booking/Services/Interfaces/IAtcPositionBookingService.cs
@@ -4,9 +4,9 @@ namespace Booking.Services.Interfaces
 {
     public interface IAtcPositionBookingService
     {
-        public Task AddAtcPositionBooking(AtcPositionBooking booking);
+        public void AddAtcPositionBooking(AtcPositionBooking booking);
         public Task<List<AtcPositionBooking>> GetAtcPositionBookings(EventAtcPosition eventAtcPosition);
-        public Task UpdateAtcPositionBooking(AtcPositionBooking booking);
-        public Task RemoveAtcPositionBooking(AtcPositionBooking booking);
+        public void UpdateAtcPositionBooking(AtcPositionBooking booking);
+        public void RemoveAtcPositionBooking(AtcPositionBooking booking);
     }
 }
