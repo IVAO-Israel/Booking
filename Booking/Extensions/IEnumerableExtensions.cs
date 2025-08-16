91431af2317f8e861eb814e69ac537e2a3ecb7c5
ivaoil <163436202+ivaoil@users.noreply.github.com>
2025-08-16 13:37:08 +0200

    Revert "Order by first 4 letters"
    This reverts commit 65e116d8ed773a9ea735d13768aa2967773f4cc9.

--- Booking/Extensions/IEnumerableExtensions.cs
+++ Booking/Extensions/IEnumerableExtensions.cs
@@ -18,14 +18,6 @@ namespace Booking.Extensions
             return source.OrderBy(item =>
             {
                 var value = selector(item);
-                if (value is not null && value.Length >= 4)
-                {
-                    return value[0..4];
-                }
-                return value;
-            }).ThenBy(item =>
-            {
-                var value = selector(item);
                 var match = AtcRegex.Match(value);
                 if (match.Success && AtcPositionOrder.TryGetValue(match.Value, out int order))
                 {
@@ -33,7 +25,7 @@ namespace Booking.Extensions
                 }
                 // If no match, sort after known positions
                 return int.MaxValue;
-            });
+            }).ThenBy(selector);
         }
     }
 }
