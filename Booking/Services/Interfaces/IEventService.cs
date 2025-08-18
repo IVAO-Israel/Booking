bdb7b930e2e58b36d6f1216585ad918c3baf354c
ivaoil <borna.cesarec@ivao.aero>
2025-08-18 06:18:19 +0200

    Events using divisionId parameter

--- Booking/Services/Interfaces/IEventService.cs
+++ Booking/Services/Interfaces/IEventService.cs
@@ -6,12 +6,12 @@ namespace Booking.Services.Interfaces
     {
         public Task AddEvent(Event eventObj);
         public Task<Event?> GetEvent(Guid id);
-        public Task<Event?> GetEventByUrl(string url);
+        public Task<Event?> GetEventByUrl(string divisionId, string url);
         public Task UpdateEvent(Event eventObj);
         public Task RemoveEvent(Event eventObj);
         public Task LoadAvailableAtcPositions(Event eventObj);
-        public Task<List<Event>> GetAllEvents();
-        public Task<List<Event>> GetUpcomingEvents();
-        public Task<List<Event>> GetUpcomingEventsForAtc();
+        public Task<List<Event>> GetAllEvents(string divisionId);
+        public Task<List<Event>> GetUpcomingEvents(string divisionId);
+        public Task<List<Event>> GetUpcomingEventsForAtc(string divisionId);
     }
 }
