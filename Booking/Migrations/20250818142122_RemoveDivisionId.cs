e00cb746f6591ec2e7988dda0b6c8b54c5f45e9e
ivaoil <borna.cesarec@ivao.aero>
2025-08-18 16:32:09 +0200

    Remove divisionid

---
+++ Booking/Migrations/20250818142122_RemoveDivisionId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDivisionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DivisionId",
+                table: "EventAtcPositions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DivisionId",
+                table: "EventAtcPositions",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
